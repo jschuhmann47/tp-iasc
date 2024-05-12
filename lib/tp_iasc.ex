f58d755aa5ebb8f4636f56474ebb36511da40ee5
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-05-12 14:12:44 -0300

    add hello world template

---
+++ lib/tp_iasc.ex
@@ -0,0 +1,20 @@
+defmodule TpIasc do
+  @moduledoc """
+  Documentation for `TpIasc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TpIasc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+    IO.puts("hello")
+    IO.puts("world")
+  end
+end
