e8b345ac8b79df907743a5fa9f0255fbf6adf3d0
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-06-20 14:12:45 -0300

    try to add multiple clients

--- lib/clientes/cliente_supervisor.ex
+++ lib/clientes/cliente_supervisor.ex
@@ -8,9 +8,13 @@ defmodule Clientes.ClienteSupervisor do
   end
 
   def init(_arg) do
-    children = [
-      {Plug.Cowboy, scheme: :http, plug: Clientes.ClienteHandler, port: 8080}
-    ]
+    children =
+      #for i <- 1..@cantidad_clientes do
+        [{Plug.Cowboy, scheme: :http, plug: Clientes.ClienteHandler, port: 8080}]
+      #end
+      #  bad child specification, more than one child specification has the id: {:ranch_listener_sup, Clientes.ClienteHandler.HTTP}.
+      # If using maps as child specifications, make sure the :id keys are unique.
+      # If using a module or {module, arg} as child, use Supervisor.child_spec/2 to change the :id,
 
     opts = [strategy: :one_for_one, name: Clientes.ClienteSupervisor]
     Supervisor.init(children, opts)
