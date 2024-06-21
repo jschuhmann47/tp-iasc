3c7283193194fe602124204e4d24ae168f28ce03
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-06-21 09:12:37 -0300

    use one client only

--- lib/clientes/cliente_supervisor.ex
+++ lib/clientes/cliente_supervisor.ex
@@ -1,20 +1,15 @@
 defmodule Clientes.ClienteSupervisor do
   use Supervisor
 
-  @cantidad_clientes 3
-
   def start_link(_etc) do
     Supervisor.start_link(__MODULE__, [])
   end
 
   def init(_arg) do
     children =
-      #for i <- 1..@cantidad_clientes do
-        [{Plug.Cowboy, scheme: :http, plug: Clientes.ClienteHandler, port: 8080}]
-      #end
-      #  bad child specification, more than one child specification has the id: {:ranch_listener_sup, Clientes.ClienteHandler.HTTP}.
-      # If using maps as child specifications, make sure the :id keys are unique.
-      # If using a module or {module, arg} as child, use Supervisor.child_spec/2 to change the :id,
+      [
+        {Plug.Cowboy, scheme: :http, plug: Clientes.ClienteHandler, options: [port: 8080]}
+      ]
 
     opts = [strategy: :one_for_one, name: Clientes.ClienteSupervisor]
     Supervisor.init(children, opts)
