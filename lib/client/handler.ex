5ac868c79d7f2707617a11ef8fd77cdc8a55c707
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-07-03 21:48:54 -0300

    fix somewhat client calls

--- lib/client/handler.ex
+++ lib/client/handler.ex
@@ -1,26 +1,21 @@
 defmodule Clients.ClientHandler do
+  alias Client.OrchestratorCaller
   use Plug.Router
-
-  @master Orchestrators.Orchestrator
-
-  # untested, but something like this (don't know how to inject these orchestrators)
-  def init(orchestrators) do
-    @master = Enum.filter(orchestrators, fn {pid, _} -> GenServer.call(pid, :is_master) end) |> List.first()
-  end
 
   plug(:match)
   plug(:dispatch)
+
   get "/ping" do
     send_resp(conn, 200, "pong")
   end
 
   get "/:key" do
-    res = GenServer.call(@master, {:get, key})
+    res = OrchestratorCaller.get_key(key)
     send_resp(conn, 200, "Got #{res}")
   end
 
   put "/:key/:value" do
-    GenServer.cast(@master, {:put, key, value})
+    OrchestratorCaller.put_key(key, value)
     send_resp(conn, 201, "Updated key #{key} with value #{value}")
   end
 
---
+++ lib/client/orchestrator_caller.ex
@@ -0,0 +1,10 @@
+defmodule Client.OrchestratorCaller do
+
+  def get_key(key) do
+    "test"
+  end
+
+  def put_key(key, value) do
+    :ok
+  end
+end
