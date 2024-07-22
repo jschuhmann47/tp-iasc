920f9f115fab780e796c50bab853780828f1c033
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-07-22 08:53:39 -0300

    add error validation to GetMaster

--- lib/clients/get_master.ex
+++ lib/clients/get_master.ex
@@ -13,13 +13,22 @@ defmodule Clients.GetMaster do
     {_, %{master: orchestrator_stored}} =
       Agent.get(
         __MODULE__,
-        &Map.get_and_update(&1, :master, fn x ->
-          case x do
+        &Map.get_and_update(&1, :master, fn current_orchestrator ->
+          case current_orchestrator do
             nil ->
               {:master, Helpers.get_master()}
 
-            x ->
-              x
+            current_orchestrator ->
+              try do
+                case GenServer.call(current_orchestrator, :ping) do
+                  # is alive so we use the one that's stored
+                  :pong -> current_orchestrator
+                  # means that it's down so we need to get the new master
+                  _ -> {:master, Helpers.get_master()}
+                end
+              catch
+                _ -> {:master, Helpers.get_master()}
+              end
           end
         end)
       )
